748ada311f8736cadd230f38b66f4ae95da42f55
rkunche <kunche.raju@gmail.com>
2019-10-27 01:57:37 +0530

    log

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,8 +9,8 @@ module.exports = {
     autorestart: true,
     watch: false,
 	max_memory_restart: '1G',
-	output: '/var/log/testnode.js/api.log',
-	error: '/var/log/testnode.js/api.log',
+	output: 'log/api.log',
+	error: 'error/api.log',
     env: {
       NODE_ENV: 'development'
     },
